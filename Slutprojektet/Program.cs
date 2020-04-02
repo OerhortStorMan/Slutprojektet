89f7d4e8f551fd2f4a5c6fcd6a8489d94b6e157c
OerhortStorMan <l.winelid@gmail.com>
2020-04-02 11:17:00 +0200

    Update Program.cs

--- Slutprojektet/Program.cs
+++ Slutprojektet/Program.cs
@@ -10,22 +10,147 @@ namespace Slutprojektet
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("\n Du har öppnat Loves Kalkylator.");
+            bool programRunning = true;
+            while (programRunning)
+            {
 
-            //välkommen till min butik
+                Console.WriteLine("\n Vad vill du räkna ut?");
+                Console.WriteLine(@"
+    1:[+]   2:[-]
+    3:[/]   4:[*]
+");
+                //bool success = int.TryParse(input, out int converted);
 
-            //(köper två saker som kostar pris)
+                bool successfulInput = false;
+                int inputInt = 0;
+                while (successfulInput == false)
+                {
+                    Console.WriteLine("\n Svara gärna mellan alternativen 1, 2, 3 eller 4.");
+                    string input = Console.ReadLine();
 
-            //skiver totalpris
+                    if (!int.TryParse(input, out inputInt))
+                    {
+                        Console.WriteLine("\n Endast siffor, tack.");
+                    }
+                    else
+                    {
+                        if (inputInt > 4 || inputInt < 1)
+                        {
+                            Console.WriteLine("Endast siffror mellan 1-4, tack.");
+                        }
+                        else
+                        {
+                            successfulInput = true;
+                        }
+                    }
+                }
+
+                if (inputInt == 1)
+                {
+
+
+                    bool successfulAmount = false;
+                    int amountInt = 0;
+                    while (successfulAmount == false)
+                    {
+                        Console.WriteLine("\n Hur många tal vill du addera?");
+                        string input = Console.ReadLine();
+
+                        if (!int.TryParse(input, out amountInt))
+                        {
+                            Console.WriteLine("\n Endast siffor, tack.");
+                        }
+                        else
+                        {
+                            if (amountInt < 1)
+                            {
+                                Console.WriteLine("Endast siffror över 1, tack.");
+                            }
+                            else
+                            {
+                                successfulAmount = true;
+                            }
+                        }
+                    }
+
+                    int[] amount = new int[amountInt];
+                    Console.WriteLine("Vänligen skriv ut talen");
+
+                    for (int i = 0; i != amountInt; i++)
+                    {
+                        int num = i + 1;
+
+                        bool success = false;
+                        int element = 0;
+                        while (success == false)
+                        {
+                            Console.WriteLine("\n" + num + ":");
+                            string input = Console.ReadLine();
+
+                            if (!int.TryParse(input, out element))
+                            {
+                                Console.WriteLine("\n Endast siffor, tack.");
+                            }
+                            else
+                            {
+
+                                success = true;
+                                Console.WriteLine();
+
+                            }
+                        }
+
+                        amount[i] = element;
+                    }
+
+                    int sum = SummarizePlus(amount);
+
+                    Console.WriteLine(sum);
+
+                    Console.WriteLine(amount[0] + " " + amount[3]);
+
+                }
+
+                else if (inputInt == 2)
+                {
+
+                }
+
+                else if (inputInt == 3)
+                {
+
+                }
+
+                else if (inputInt == 4)
+                {
+
+                }
+
+                Console.WriteLine("success");
+                Console.ReadLine();
+
+            }
         }
 
-        static void X()
+        static int SummarizePlus(int[] amount)
+        {
+            int sum = 0;
+            for (int i = 0; i < amount.Length; i++)
+            {
+                sum = sum + amount[i];
+            }
+
+            return sum;
+        }
+
+        /*static int Plus()
         {
             //(multiplicerar pris med antal och returnerar total pris
         }
 
 
-        //parametrar skadar min hjärna, fattar *0*
+        //parametrar skadar min hjärna, fattar *0**/
     }
 }
 
